a65752625fddf77a7c704712aaab0f343e3f0def
Sohail Ansari <sohail.ai.work@gmail.com>
2026-08-25 10:57:12 +0530

    Create day1_practice.py

---
+++ Python/Practice/day1_practice.py
@@ -0,0 +1,22 @@
+# Question 1: Write a Program to input 2 numbers & print their sum
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+print("the sum of", num1, "and", num2, "is", num1 + num2)
+
+
+# Question 2: WAP to input side of a square & print its area
+side = float(input("Enter the side of the square in cm: "))
+print("The area of the square is", side * side, "cm²")
+
+
+# Question 3: WAP to input 2 floating point numbers & print their average
+num1 = float(input("Enter first floating point number:"))
+num2 = float(input("Enter second floating point number:"))
+average = (num1 + num2)/2
+print("The average of", num1, "and", num2, "is", average)
+
+
+# Question 4: WAP to input 2 int numbers, a and b. Print True if a is greater than or equal to b. If not print False.
+a = int(input("enter first number: "))
+b = int(input("enter second number:"))
+print(a >= b)
