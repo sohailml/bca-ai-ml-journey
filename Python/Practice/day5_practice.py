910a35a2bab8c1f0345155d0fd0e24624cb02423
Sohail Ansari <sohail.ai.work@gmail.com>
2026-08-28 22:30:52 +0530

    Add day5_practice.py with initial practice questions

---
+++ Python/Practice/day5_practice.py
@@ -0,0 +1,27 @@
+"""Q1: Print numbers from 1 to 100"""
+# i = 1
+# while i<=100:
+#     print(i)
+#     i+=1
+# print("end")
+
+
+"""Q2: Print numbers from 100 to 1."""
+# j = 100
+# while j>=1:
+#     print(j)
+#     j-=1
+# print("end")
+
+
+"""Q3: Print the multiplication table of a number n."""
+# n = int(input("enter a number: "))
+# i = 1
+# while i<=10:
+#     print(n*i)
+#     i += 1
+# print("end")
+
+
+"""Q4: """
+#coming soon..........
