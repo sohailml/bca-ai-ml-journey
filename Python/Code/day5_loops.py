d54da0f17d1bc7fb35a28da9b6fa71fad0085d83
Sohail Ansari <sohail.ai.work@gmail.com>
2026-08-28 22:29:44 +0530

    Add loops example in day5_loops.py

---
+++ Python/Code/day5_loops.py
@@ -0,0 +1,22 @@
+# day 5 of my jurney
+# loops in python
+i = 1
+while i<=5: #printing value while condition is true
+    print("sohail")
+    i += 1
+print("ended")
+
+j = 1
+while j<=10:
+    print(j)
+    j += 1
+print("ended")
+
+k = 5
+while k>=1:
+    print(k)
+    k -= 1
+print("ended")
+
+
+
